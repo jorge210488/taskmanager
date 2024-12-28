e65f3143138292d30501dacb2eaf1cc11e0dfebd
jorge <jorgemartinez.jam@gmail.com>
2024-12-28 03:48:37 -0300

    fix: swagger description

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -19,7 +19,26 @@ async function bootstrap() {
 
   const config = new DocumentBuilder()
     .setTitle('Backend API')
-    .setDescription('API documentation for backend')
+    .setDescription(
+      `
+      Bienvenido a la documentación de la API del Backend. Esta aplicación está desarrollada utilizando TypeScript y el framework NestJS, siguiendo las mejores prácticas para construir un backend escalable y mantenible.
+  
+      ### Características Principales:
+      - **Módulo de Autenticación**: Permite el registro de usuarios, inicio de sesión y acceso seguro a los recursos mediante autenticación basada en JWT.
+      - **Módulo de Tareas (Task)**: Proporciona funcionalidades para la gestión de tareas, incluyendo creación, actualización, consulta y eliminación, con soporte para filtrar por estado (completadas o pendientes).
+  
+      ### Base de Datos:
+      La aplicación utiliza MongoDB como base de datos, ofreciendo alto rendimiento y flexibilidad para manejar datos estructurados y semi-estructurados.
+  
+      ### Herramientas y Utilidades:
+      - Integración con Swagger para la documentación y pruebas de la API.
+      - Validación de datos mediante pipes globales para garantizar la integridad.
+      - Filtros de excepciones personalizados para un manejo uniforme de errores.
+  
+      ### Introducción:
+      Para interactuar con la API, utiliza los endpoints que se describen a continuación. Asegúrate de estar autenticado con un token Bearer válido para acceder a las rutas protegidas.
+    `,
+    )
     .setVersion('1.0')
     .addBearerAuth()
     .build();
